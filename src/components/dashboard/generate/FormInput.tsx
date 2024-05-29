ca72f04ba67e578fbecbbe94a2fa962c00c94708
Mohammad-Shahid-07 <112233075+Mohammad-Shahid-07@users.noreply.github.com>
2024-05-29 15:24:49 +0530

    added user generation limit

--- src/components/dashboard/generate/FormInput.tsx
+++ src/components/dashboard/generate/FormInput.tsx
@@ -2,7 +2,7 @@
 
 'use client';
 
-import { FC, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { TypeInteriorDesign } from '@/types/types';
 import { supabaseBrowserClient } from '@/utils/supabase/client';
 import { useRouter } from 'next/navigation';
@@ -19,6 +19,8 @@ import { Badge } from '@/components/ui/badge';
 import { cn, errorToast } from '@/utils/utils';
 import { SubmitButton } from '@/components/SubmitButton';
 import { roomOptions, roomThemes } from './content';
+import { toast } from '@/components/ui/use-toast';
+import ModalLimitExceeded from '@/components/dashboard/generate/ModalLimitExceeded';
 
 type FormInputProps = {
   data?: TypeInteriorDesign;
@@ -33,7 +35,29 @@ type FormFields = {
 
 const FormInput: FC<FormInputProps> = ({ data }) => {
   const supabase = supabaseBrowserClient();
+
+  // State to check if the user has reached the limit of content creations
+  const [limitExceeded, setIsLimitExceeded] = useState(false);
   const router = useRouter();
+
+  //function to check the limit of content creations and set the state accordingly
+  const limitUser = useCallback(async () => {
+    const { error, count } = await supabase
+      .from('interior_designs')
+      .select('*', { count: 'exact', head: true });
+
+    if (error) {
+      return toast({ description: error.message, variant: 'destructive' });
+    }
+    if (count && count >= 5) {
+      setIsLimitExceeded(true);
+    }
+  }, []);
+
+  //checking on load if the user has reached the limit of content creations
+  useEffect(() => {
+    limitUser();
+  }, [limitUser]);
 
   //TODO change the initial data to match the data structure
   const initialData: FormFields = {
@@ -140,6 +164,7 @@ const FormInput: FC<FormInputProps> = ({ data }) => {
 
   return (
     <div>
+      <ModalLimitExceeded isModalOpen={limitExceeded} />
       <p className='text-default font-semibold mb-2'>Let’s create a room</p>
       <div className='block md:flex gap-4'>
         <div className='border p-4 rounded-lg w-full md:w-2/5 lg:w-3/12'>
@@ -219,7 +244,11 @@ const FormInput: FC<FormInputProps> = ({ data }) => {
             </InputWrapper>
 
             {/* Generate button */}
-            <SubmitButton className='w-full' isLoading={isLoading} formAction={handleGeneration}>
+            <SubmitButton
+              className='w-full'
+              isLoading={isLoading}
+              disabled={limitExceeded}
+              formAction={handleGeneration}>
               Generate Image
             </SubmitButton>
           </form>
